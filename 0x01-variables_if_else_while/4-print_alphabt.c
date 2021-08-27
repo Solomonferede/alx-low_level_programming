79c2afbba7be34b149f7a8cf350abe4186e21e45
Solomonferede <ezezsolomonferede@gmail.com>
2021-08-27 06:16:48 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase, followed by
+ * a new line.
+ * Return: 0 if exited without error, non-zero otherwise.
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != q || ch != e)
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
