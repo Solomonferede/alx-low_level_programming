262e3992ba7cbaf5b5aa7d88290715e80b3d71a5
Solomonferede <ezezsolomonferede@gmail.com>
2021-08-31 12:14:13 -0700

    multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,11 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
-				printf("%d, ", i * j);
+			{
+				printf("%d", i * j);
+				if (j != n)
+					printf(",  ")
+			}
 			printf("\n");
 		}
 	}
