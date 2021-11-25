240b8b738950aa08f1eaec4aa5154c5847577bda
Solomonferede <ezezsolomonferede@gmail.com>
2021-11-25 22:25:33 +0300

    ADDED header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
- Info
-Please use this data structure for this project:
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -11,13 +11,12 @@ Please use this data structure for this project:
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- *
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -31,3 +30,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
+
