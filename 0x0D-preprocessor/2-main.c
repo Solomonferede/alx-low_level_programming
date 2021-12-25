1d70d3a7dbf9276badfe5a341d07ec4759230a7c
Solomonferede <ezezsolomonferede@gmail.com>
2021-12-25 08:31:42 +0300

    Added a file that prints the file compiling

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - print name of compling file
+ * return: 0 if sucess , non-zero otherwise
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
