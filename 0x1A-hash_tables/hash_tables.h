5469c2766ea3756d05371cc4fab4645f10a792db
Solomonferede <ezezsolomonferede@gmail.com>
2021-12-28 10:27:55 +0300

    Header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,10 @@
-#ifndef FILE_HASH_TABLES_H
-#define FILE_HASH_TABLES_H
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
